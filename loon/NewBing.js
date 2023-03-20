9b0d5cc0bb704a689b4e0b869366d14e523d1129
Zhou Qiuyu <1411770819@qq.com>
2023-03-20 20:01:36 +0800

    添加cn.bing.com替换为www.bing.com

--- loon/NewBing.js
+++ loon/NewBing.js
@@ -1,3 +1,5 @@
 $request.headers['User-Agent']="AppleWebKit/537.36 Chrome/110.0 Safari/537.36 Edg/110.0"
+// 如果url为cn.bing.com则替换为www.bing.com
+$request.url=$request.url.replace(/cn.bing.com/g,"www.bing.com")
 
 $done($request)
